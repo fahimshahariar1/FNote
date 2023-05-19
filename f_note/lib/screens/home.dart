ee5c43975c99c63636a8c69433f879d860a4853e
Fahim <fahimshahariar.fs@gmail.com>
2023-05-20 00:52:50 +0600

    first and half

--- f_note/lib/screens/home.dart
+++ f_note/lib/screens/home.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:f_note/style/appstyle.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,7 +14,41 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Appstyle.mainColor,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text("F Notes", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Appstyle.mainColor,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Your recent notes",
+            style: GoogleFonts.roboto(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22),
+          ),
+          SizedBox(height: 20,),
+          StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection("fnotes").snapshots(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.connectionState == ConnectionState) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (snapshot.hasData) {
+                return GridView(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),);
+              }
+              return Text("There's no notes", style: GoogleFonts.nunito,)
+            },
+          )
+        ],
+      ),
     );
   }
 }
